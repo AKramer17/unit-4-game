6d06380256f19e5272e59cff604ae845b3c7485e
Alex Kramer <kramer612@gmail.com>
2018-11-21 19:25:09 -0500

    plotting javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    var current = 0;
+    var winCount = 0;
+    var lossCount = 0;
+    var target = Math.floor(Math.random() * 150 + 39);
+
+    $("#targetNumber").text(target);
+
+    var valC1 = Math.floor(Math.random() * 17 + 1);
+    var valC2 = Math.floor(Math.random() * 17 + 1);
+    var valC3 = Math.floor(Math.random() * 17 + 1);
+    var valC4 = Math.floor(Math.random() * 17 + 1);
+
+    $("#cstl1").on("click", function() {
+        current += valC1;
+    });
+
+    $("#cstl2").on("click", function() {
+        current += valC2;
+    });
+
+    $("#cstl3").on("click", function() {
+        current += valC3;
+    });
+
+    $("#cstl4").on("click", function() {
+        current += valC4;
+    });
+});
