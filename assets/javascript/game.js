7a38adb63e1b9c84535779c5186a0bba82b9b2c8
Alex Kramer <kramer612@gmail.com>
2018-11-21 21:07:48 -0500

    fully functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,23 +16,65 @@ $(document).ready(function() {
         current += valC1;
         $("#playerNumber").text(current);
         if (current === target) {
-            alert("You win! Yay math!");
-            winCount++;
+            win();
         } else if (current > target) {
-            alert("You lose! But let's try again")
-            lossCount++;
+            lose();
         }
     });
 
     $("#cstl2").on("click", function() {
         current += valC2;
+        $("#playerNumber").text(current);
+        if (current === target) {
+            win();
+        } else if (current > target) {
+            lose();
+        }
     });
 
     $("#cstl3").on("click", function() {
         current += valC3;
+        $("#playerNumber").text(current);
+        if (current === target) {
+            win();
+        } else if (current > target) {
+            lose();
+        }
     });
 
     $("#cstl4").on("click", function() {
         current += valC4;
+        $("#playerNumber").text(current);
+        if (current === target) {
+            win();
+        } else if (current > target) {
+            lose();
+        }
     });
+
+    function win() {
+        alert("You win! Yay math!");
+        winCount++;
+        $("#wins").text(winCount);
+        reset();
+    }
+
+    function lose() {
+        alert("You lose! But let's try again");
+        lossCount++;
+        $("#losses").text(lossCount);
+        reset();
+    }
+
+    function reset() {
+        target = Math.floor(Math.random() * 150 + 39);
+        $("#targetNumber").text(target);
+        valC1 = Math.floor(Math.random() * 17 + 1);
+        valC2 = Math.floor(Math.random() * 17 + 1);
+        valC3 = Math.floor(Math.random() * 17 + 1);
+        valC4 = Math.floor(Math.random() * 17 + 1);
+        current = 0;
+        $("#playerNumber").text(current);
+      }
+
 });
